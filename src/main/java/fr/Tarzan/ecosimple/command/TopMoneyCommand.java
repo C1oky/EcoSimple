96e4107e1acbab947c4a6fec39c87f3cae9d4219
DalkerTM <32621158+GabaiII@users.noreply.github.com>
2020-05-17 18:35:24 +0200

    Add files via upload

--- src/main/java/fr/Tarzan/ecosimple/command/TopMoneyCommand.java
+++ src/main/java/fr/Tarzan/ecosimple/command/TopMoneyCommand.java
@@ -5,10 +5,12 @@ import cn.nukkit.command.CommandSender;
 import cn.nukkit.utils.Config;
 import fr.Tarzan.ecosimple.Loader;
 import fr.Tarzan.ecosimple.components.LanguageUtil;
+import fr.Tarzan.ecosimple.components.MoneyAPI;
 
 public class TopMoneyCommand extends Command {
 
-    private final Config config = Loader.getInstance().getConfig();
+    private final MoneyAPI money = Loader.getMoneyApi();
+    private final Loader loader = Loader.getInstance();
 
     public TopMoneyCommand() {
         super("topmoney", LanguageUtil.translate("command.topmoney.description"), "/topmoney");
@@ -17,7 +19,7 @@ public class TopMoneyCommand extends Command {
 
     @Override
     public boolean execute(CommandSender sender, String s, String[] args) {
-        //TODO: Will be implemented in the future...
+        sender.sendMessage(LanguageUtil.translate("command.topmoney.presentation")+ "\n\n" + money.getFormattedTopMoney(loader.getConfig().getInt("topmoney.playercount")));
         return true;
     }
 }
