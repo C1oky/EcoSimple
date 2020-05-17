608d4f94e20451d0f78b7cf813d648414715d96a
DalkerTM <32621158+GabaiII@users.noreply.github.com>
2020-05-17 18:35:53 +0200

    Add files via upload

--- src/main/java/fr/Tarzan/ecosimple/components/MoneyAPI.java
+++ src/main/java/fr/Tarzan/ecosimple/components/MoneyAPI.java
@@ -3,7 +3,15 @@ package fr.Tarzan.ecosimple.components;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.utils.Config;
+import fr.Tarzan.ecosimple.Loader;
 import fr.Tarzan.ecosimple.event.PlayerCreateAccountEvent;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.zip.DeflaterOutputStream;
 
 public class MoneyAPI {
 
@@ -11,6 +19,7 @@ public class MoneyAPI {
     private static final Config config = new Config(configPath, Config.JSON);
     private static double defaultMoney = config.getDouble("default", 1000);
     private static MoneyAPI instance;
+    private static Loader loader;
 
     public MoneyAPI() {
         instance = this;
@@ -71,15 +80,47 @@ public class MoneyAPI {
         config.save(true);
     }
 
+    public String getFormattedTopMoney(int playersCount) {
+
+        Map<String, Double> counter = calculateMoney();
+        String formattedTop = "";
+        int viewN = 1;
+
+        for(Map.Entry<String, Double> entry : counter.entrySet()) {
+            String name = entry.getKey();
+            Double money = entry.getValue();
+            formattedTop = formattedTop+ LanguageUtil.translate("command.topmoney.subtitle",viewN,name,money) + "\n";
+        viewN++;
+
+        if (viewN == playersCount) {
+            return formattedTop;
+        }
+    }
+
+        return formattedTop;
+    }
+
     public double getDefaultMoney() {
         return defaultMoney;
     }
 
-    public String getFormattedTopMoney(int playersCount) {
-        return "";
-    }
-
     public static MoneyAPI getInstance() {
         return instance;
+    }
+
+    private Map<String, Double> calculateMoney() {
+        HashMap<String, Double> map = new HashMap<>();
+        for (Map.Entry<String, Object> entry : config.getAll().entrySet()) {
+            map.put(entry.getKey(), (Double) entry.getValue());
+        }
+        Map<String, Double> sorted = map;
+        sorted = map
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                        LinkedHashMap::new));
+
+        return sorted;
     }
 }
